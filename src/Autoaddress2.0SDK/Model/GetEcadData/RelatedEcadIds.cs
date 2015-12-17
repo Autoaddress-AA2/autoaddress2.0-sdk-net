ed693c12fc39a26530b9cd5c97009dbea9b04b42
alanmartin0 <alan.martin@autoaddress.ie>
2015-12-17 12:56:24 +0000

    RelatedEcadIds changes

--- src/Autoaddress2.0SDK/Model/GetEcadData/RelatedEcadIds.cs
+++ src/Autoaddress2.0SDK/Model/GetEcadData/RelatedEcadIds.cs
@@ -8,10 +8,59 @@ namespace Autoaddress.Autoaddress2_0.Model.GetEcadData
     public class RelatedEcadIds
     {
         [JsonConstructor]
-        internal RelatedEcadIds(int[] geographicCountyEcadIds)
+        internal RelatedEcadIds(int? addressPointEcadId,
+                                int? buildingEcadId,
+                                int? buildingGroupEcadId,
+                                int[] thoroughfareEcadIds,
+                                int[] localityEcadIds,
+                                int[] postTownEcadIds,
+                                int[] postCountyEcadIds,
+                                int[] geographicCountyEcadIds)
         {
+            AddressPointEcadId = addressPointEcadId;
+            BuildingEcadId = buildingEcadId;
+            BuildingGroupEcadId = buildingGroupEcadId;
+            ThoroughfareEcadIds = thoroughfareEcadIds;
+            LocalityEcadIds = localityEcadIds;
+            PostTownEcadIds = postTownEcadIds;
+            PostCountyEcadIds = postCountyEcadIds;
             GeographicCountyEcadIds = geographicCountyEcadIds;
         }
+        
+        /// <summary>
+        /// Gets the address point ECAD id.
+        /// </summary>
+        public int? AddressPointEcadId { get; private set; }
+        
+        /// <summary>
+        /// Gets the building ECAD id.
+        /// </summary>
+        public int? BuildingEcadId { get; private set; }
+
+        /// <summary>
+        /// Gets the building group ECAD ids.
+        /// </summary>
+        public int? BuildingGroupEcadId { get; private set; }
+
+        /// <summary>
+        /// Gets the thoroughfare ECAD ids.
+        /// </summary>
+        public int[] ThoroughfareEcadIds { get; private set; }
+        
+        /// <summary>
+        /// Gets the locality ECAD ids.
+        /// </summary>
+        public int[] LocalityEcadIds { get; private set; }
+
+        /// <summary>
+        /// Gets the post town ECAD ids.
+        /// </summary>
+        public int[] PostTownEcadIds { get; private set; }
+
+        /// <summary>
+        /// Gets the post county ECAD ids.
+        /// </summary>
+        public int[] PostCountyEcadIds { get; private set; }
 
         /// <summary>
         /// Gets the geographic county ECAD ids.
