91a7bb7fcb0b79292f387feaf5452aae498d5ab4
alanmartin0 <alan.martin@autoaddress.ie>
2017-03-30 13:03:50 +0800

    AssemblyInfo changes

--- src/Autoaddress2.0SDK/Properties/AssemblyInfo.cs
+++ src/Autoaddress2.0SDK/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -11,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Autoaddress2.0SDK")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
