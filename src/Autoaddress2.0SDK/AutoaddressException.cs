ccee7a0c918874a4fcf2ae11a10bc3a0cd8deb86
Petar Repac <petar.repac@autoaddress.ie>
2017-11-17 11:04:35 +0000

    better AutoaddressException

--- src/Autoaddress2.0SDK/AutoaddressException.cs
+++ src/Autoaddress2.0SDK/AutoaddressException.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using Autoaddress.Autoaddress2_0.Model;
 
 namespace Autoaddress.Autoaddress2_0
@@ -11,12 +12,30 @@ namespace Autoaddress.Autoaddress2_0
         /// <summary>
         /// Gets the type of the error.
         /// </summary>
-        public ErrorType ErrorType { get; private set; }
+        public ErrorType ErrorType { get; }
 
-        internal AutoaddressException(ErrorType errorType, string message)
+        /// <summary>
+        /// Gets the status code of response.
+        /// </summary>
+        public HttpStatusCode HttpStatusCode { get; }
+
+        /// <summary>
+        /// Gets request Uri.
+        /// </summary>
+        public Uri RequestUri { get; }
+
+        internal AutoaddressException(ErrorType errorType, HttpStatusCode httpStatusCode, Uri requestUri, string message)
             : base(message)
         {
             ErrorType = errorType;
+            HttpStatusCode = httpStatusCode;
+            RequestUri = requestUri;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"ErrorType=[{ErrorType}], HttpStatusCode=[{HttpStatusCode}], RequestUri=[{RequestUri}], {base.ToString()}";
         }
     }
 }
--- src/Autoaddress2.0SDK/Helpers/HttpRequestHelper.cs
+++ src/Autoaddress2.0SDK/Helpers/HttpRequestHelper.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Autoaddress.Autoaddress2_0.Model;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Autoaddress.Autoaddress2_0.Model;
-using Newtonsoft.Json.Linq;
 
 namespace Autoaddress.Autoaddress2_0.Helpers
 {
@@ -13,45 +14,56 @@ namespace Autoaddress.Autoaddress2_0.Helpers
             HttpResponseMessage response = await httpClient.GetAsync(requestUri).ConfigureAwait(false);
             string result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
 
-            if ((int) response.StatusCode == 429)
+            if (response.IsSuccessStatusCode)
+            {
+                return result;
+            }
+
+            if ((int)response.StatusCode == 429)
             {
                 throw new TooManyRequestsException(response.ReasonPhrase);
             }
 
-            AutoaddressException autoaddressException = GetAutoaddressException(result);
+            AutoaddressException autoaddressException = GetAutoaddressException(response.StatusCode, requestUri, result);
 
             if (autoaddressException != null)
             {
                 throw autoaddressException;
             }
 
+            // guard
             response.EnsureSuccessStatusCode();
 
-            return result;
+            throw new InvalidOperationException();
         }
 
-        private static AutoaddressException GetAutoaddressException(string response)
+        private static AutoaddressException GetAutoaddressException(HttpStatusCode httpStatusCode, Uri requestUri, string response)
         {
             JObject obj;
             try
             {
                 obj = JObject.Parse(response);
             }
-            catch (Exception e)
+            catch
             {
-                throw new Exception($"Error while JObject.Parse(response), response=[{response}]", e);
+                return new AutoaddressException(ErrorType.Unknown, httpStatusCode, requestUri, $"response=[{response}]");
             }
 
-            AutoaddressException autoaddressException = null;
-
-            if (obj["errors"] != null && obj["errors"].HasValues && obj["errors"][0]["type"]["code"] != null && obj["errors"][0]["message"] != null)
+            try
             {
-                ErrorType errorType = (ErrorType)((int)obj["errors"][0]["type"]["code"]);
-                string message = obj["errors"][0]["message"].ToString();
-                autoaddressException = new AutoaddressException(errorType, message);
+                AutoaddressException autoaddressException = null;
+                if (obj["errors"] != null && obj["errors"].HasValues && obj["errors"][0]["type"]["code"] != null && obj["errors"][0]["message"] != null)
+                {
+                    ErrorType errorType = (ErrorType) ((int) obj["errors"][0]["type"]["code"]);
+                    string message = obj["errors"][0]["message"].ToString();
+                    autoaddressException = new AutoaddressException(errorType, httpStatusCode, requestUri, message);
+                }
+                return autoaddressException;
             }
-
-            return autoaddressException;
+            catch
+            {
+                return new AutoaddressException(ErrorType.Unknown, httpStatusCode, requestUri, $"response=[{response}]");
+            }
         }
     }
 }
